91fec4c6e5fd52434a6834b7c76f57f0d3ac21ef
Mohammad Alshikh <95369034+mohammadalshikh@users.noreply.github.com>
2025-11-05 02:27:28 -0500

    Update sample data

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,13 @@ const sampleData = {
             position: 'Software Developer Intern',
             duration: 'Jan. 2025 - Aug. 2025',
             description: "Leveraged Coveo’s Headless and Atomic frameworks to enhance search efficiency and personal recommendations.",
-            technologies: ['Python', 'React', 'Angular', 'C#', 'Azure DevOps'],
+            technologies: [
+                'Python',
+                'React',
+                'Angular',
+                'C#',
+                'Azure DevOps'
+            ],
             image: coveo
         },
         {
@@ -39,7 +45,12 @@ const sampleData = {
             position: 'Web Developer Intern',
             duration: 'May 2024 - Aug. 2024',
             description: 'Working on key features and bug fixes to enhance product stability and user satisfaction, while also engaging in daily meetups.',
-            technologies: ['C#', 'Angular', 'JavaScript', 'SQL Server'],
+            technologies: [
+                'C#',
+                'Angular',
+                'JavaScript',
+                'SQL Server'
+            ],
             image: consoltec
         },
         {
@@ -48,7 +59,12 @@ const sampleData = {
             position: 'Software Engineer Intern',
             duration: 'Jan. 2024 - Apr. 2024',
             description: 'Collaborated with the deployment team to develop Sitecore-based websites, resolve maintenance issues, and structured bug analysis.',
-            technologies: ['C#', 'ASP.NET', 'IIS', 'SQL Server'],
+            technologies: [
+                'C#',
+                'ASP.NET',
+                'IIS',
+                'SQL Server'
+            ],
             image: sitecore
         },
     ],
@@ -68,42 +84,86 @@ const sampleData = {
     ],
     projects: [
         {
-            id: 4,
-            name: 'True Clear Terminal',
-            description: 'VS Code extension to clear integrated terminal and scroll-back buffer',
-            technologies: ['TypeScript', 'JavaScript'],
-            link: 'https://marketplace.visualstudio.com/items?itemName=mohammadalshikh.true-clear-terminal',
-            github: 'https://github.com/mohammadalshikh/true-clear-terminal-vscode',
+            id: 2,
+            name: 'BestFood',
+            description: 'Food e-commerce website',
+            technologies: [
+                'Java',
+                'Spring Boot',
+                'MySQL',
+                'Bootstrap'
+            ],
+            link: 'https://bestfood.mohammadalshikh.com',
+            github: 'https://github.com/mohammadalshikh/e-commerce-spring-boot',
         },
         {
             id: 3,
             name: 'MindSync',
             description: 'Task-management Discord bot with LLM integration',
-            technologies: ['Discord.js', 'OpenAI API'],
+            technologies: [
+                'Discord.js',
+                'OpenAI API'
+            ],
             link: null,
             github: 'https://github.com/mohammadalshikh/mindsync',
         },
         {
-            id: 2,
-            name: 'BestFood',
-            description: 'Food e-commerce website',
-            technologies: ['Java', 'Spring Boot', 'MySQL', 'Bootstrap'],
-            link: 'https://bestfood.mohammadalshikh.com',
-            github: 'https://github.com/mohammadalshikh/e-commerce-spring-boot',
+            id: 4,
+            name: 'True Clear Terminal',
+            description: 'VS Code extension to clear integrated terminal and scroll-back buffer',
+            technologies: [
+                'TypeScript',
+                'JavaScript'
+            ],
+            link: 'https://marketplace.visualstudio.com/items?itemName=mohammadalshikh.true-clear-terminal',
+            github: 'https://github.com/mohammadalshikh/true-clear-terminal-vscode',
         },
         {
             id: 1,
             name: 'ConcoGrades',
-            description: 'A classroom management web-app for teachers and students.',
-            technologies: ['Python', 'Flask', 'JavaScript', 'Bootstrap'],
+            description: 'A classroom management web-app for teachers and students',
+            technologies: [
+                'Python',
+                'Flask',
+                'JavaScript',
+                'Bootstrap'
+            ],
             link: 'https://concogrades.mohammadalshikh.com',
             github: 'https://github.com/mohammadalshikh/classroom-management-flask',
         },
+        {
+            id: 5,
+            name: "SolarScope",
+            description: "Interactive 3D solar system simulation",
+            technologies: [
+                "C++",
+                "OpenGL",
+                "Assimp"
+            ],
+            link: null,
+            github: "https://github.com/mohammadalshikh/comp-371-project",
+        }
     ],
     about: {
-        intro: `Hello! I'm Mohammad Alshikh, a passionate developer who loves creating beautiful and functional web experiences.\n\nCurrently finishing my last year at Concordia, I have learned a handful of technologies over the past few years that helped me grow and contribute as a developer. At the moment, my focus is on building modern web applications that are both user-friendly and scalable.`,
-        skills: ['Python', 'C#', 'Java', 'React', 'Angular', 'Flask', 'Spring Boot', 'SQL'],
-        interests: ['Making cool UIs', 'Movies/TV shows', 'Writing stories', 'Exploring nature', 'Literally all sports', 'Video games', 'Hip-hop'],
+        intro: `Hello! I'm Mohammad Alshikh, a passionate developer who loves creating beautiful and functional web experiences.\n\nCurrently finishing my last year at Concordia, I have learned a handful of technologies over the past few years that helped me grow and contribute as an engineer. At the moment, my focus is on building modern web applications that are both robust and scalable.`,
+        skills: [
+            'Python',
+            'C#',
+            'Java',
+            'Spring Boot',
+            'Flask',
+            'React',
+            'Angular',
+            'SQL'
+        ],
+        interests: [
+            'Writing stories',
+            'Exploring nature',
+            'Sports',
+            'Cinema',
+            'Video games',
+            'Hip-hop'
+        ],
     },
 };
 
